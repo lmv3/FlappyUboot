a7108fe2f6b9f9744a6699a3fd5bf47d34da2c1b
Rene <rene_tilinski@web.de>
2017-12-16 19:11:32 +0100

    Switching between the different scenes

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		//hitting Space makes us start the game
+		if (Input.GetKeyUp("space")){
+			SceneManager.LoadScene("FlappyUboot");
+				
+		}
+		//hitting escape closes the game
+		if (Input.GetKeyUp("escape")){
+			Application.Quit();
+		}
+	}
+}
