b53d795857bead2ee10fee2934c367d003978865
lmv3 <32383681+lmv3@users.noreply.github.com>
2017-12-19 18:45:14 +0100

    moving background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject backGround;
+    void Start () {
+        InvokeRepeating("CreateBackground", 1f, 3f);
+
+    }
+	
+	// Update is called once per frame
+	void CreateBackground () {
+        Instantiate(backGround, new Vector3(10, 0, 0), Quaternion.identity);
+
+    }
+}
