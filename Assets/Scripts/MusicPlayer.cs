4aa5f31e3e542f871f55739565ca10b286476df3
Rene <rene_tilinski@web.de>
2017-12-19 14:35:48 +0100

    Backgroundmusik hinzugefügt

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer _instance;
+
+	void Awake(){
+		DontDestroyOnLoad(this.gameObject);
+
+		if(GameObject.Find(gameObject.name) && GameObject.Find(gameObject.name) != this.gameObject){
+			Destroy(GameObject.Find(gameObject.name));
+		}
+	}
+}
