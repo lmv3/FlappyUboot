792d491e74fb1a154ee564834521a6a912574736
Rene <rene_tilinski@web.de>
2018-01-03 18:51:16 +0100

    Cooldown added to shrink ability

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -6,6 +6,13 @@ public class AnimationManager : MonoBehaviour {
 
 	public Animator animator;
 
+
+	//variable to keep track of when the ability was started the last time
+	private float shrinkStart = 0f;
+
+	//the cooldown for the ability
+	private float shrinkCooldown = 6f;
+
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -16,10 +23,16 @@ public class AnimationManager : MonoBehaviour {
 		//setting copyCount to the value of the count variable which is used in the player script to track the score of the player
 		int copyCount = Player.count;
 
+		//timeStamp = Time.time + 6.0f;
+
 		//Let the player shrink when the button is pressed and the player has a score which is higher or equal 5. Afterwards, decreasing the score of the player by 5
-		if(Input.GetKeyDown("1") && copyCount >= 5){
-			animator.Play("Shrink");
-			Player.count = Player.count - 5;
+		if(Input.GetKeyDown("1") && copyCount >= 5 ){
+			//checking if the cooldown is already over
+			if(Time.time >  shrinkStart + shrinkCooldown){
+				animator.Play("Shrink");
+				Player.count = Player.count - 5;
+				shrinkStart = Time.time;
+			}
 		}	
 	}
 }
