78f7b3d2de35cd49f84c9706eb0abce40ea42b76
DESKTOP-22C645P\Lars <l.goldmann@outlook.com>
2018-01-03 20:53:01 +0100

    scrolling background

---
+++ Assets/Scripts/BGscroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BGscroller : MonoBehaviour
+{
+    public float speed;
+    Vector3 startPOS;
+
+
+
+    void Start()
+    {
+        startPOS = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Translate((new Vector3(-1, 0, 0)) * speed * Time.deltaTime);
+
+        if (transform.position.x < -19.09307f)
+        {
+            transform.position = startPOS;
+        }
+
+    }
+}
