b479c49e8c1ecf414f9d866360197a9d4417003e
DESKTOP-22C645P\Lars <l.goldmann@outlook.com>
2018-01-07 00:32:55 +0100

    bubbles and new background

--- Assets/Scripts/GenerateCoin.cs
+++ Assets/Scripts/GenerateCoin.cs
@@ -6,9 +6,11 @@ public class GenerateCoin : MonoBehaviour
 {
     public GameObject coin;
 
+    public float start;
+    public float repeat;
     void Start()
     {   // Startzeit: 2.5s (nach den Plants), Wiederholung alle 3s
-        InvokeRepeating("CreateCoin", 2.5f, 3f);
+        InvokeRepeating("CreateCoin", start, repeat);
     }
     void CreateCoin()
     {   // instantiate Coin-Prefab 
